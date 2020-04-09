fa890179c4f7e8cd46857460939c22ff73f8e772
Symeon Quimby <kershoc@gmail.com>
2020-04-08 23:14:10 -0400

    trim message options
    message options will hold the chat message when it is a chat, trim it

--- services/twitch-chat/src/MessageObject.php
+++ services/twitch-chat/src/MessageObject.php
@@ -18,7 +18,7 @@ class MessageObject
     public function __construct($command, $options, $tags = [], $irc_user = null, $irc_room = null)
     {
         $this->command = $command;
-        $this->options = $options;
+        $this->options = trim($options, ": \t\n\r\0\x0B");
         $this->tags = $tags;
         $this->irc_user = $irc_user;
         $this->irc_room = $irc_room;
